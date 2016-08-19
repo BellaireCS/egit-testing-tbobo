1c55fb5e26bf81ff73ebc3270e8b4d35244ab50c
Melody Lam <melody.c.lam@gmail.com>
2016-08-19 10:25:50 -0500

    Seeded repo with first assignment.

---
+++ BHSStarterCode/src/ReadWriteFiles.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ReadWriteFiles {
+
+	
+	public static void main(String[] args)
+	{
+		/**
+		 * Place code here. You should have a scanner, a file object, and a PrintWriter obj
+		 * to write to an output file.
+		 */
+		
+		
+		
+	}
+}
