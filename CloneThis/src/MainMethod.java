b7548072246354427de3e34a154e7ac7918c7407
Melody Lam <melody.c.lam@gmail.com>
2016-08-24 08:01:05 -0500

    Added MainMethod.java
    Added a MainMethod.java as a test of pulling and pushing capabilities on
    student computers.

---
+++ CloneThis/src/MainMethod.java
@@ -0,0 +1,11 @@
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is a test to make sure you can pull from a repo.");
+		System.out.println("Change this line to something else. Then commit & push to your repo.");
+
+	}
+
+}
