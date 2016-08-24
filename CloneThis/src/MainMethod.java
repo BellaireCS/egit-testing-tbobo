5af22e26c34949c6e73f8b3391e37918ddb8114b
tlsong54321@gmail.com <tlsong54321@gmail.com>
2016-08-24 09:37:47 -0500

    Changed Lam's message

--- CloneThis/src/MainMethod.java
+++ CloneThis/src/MainMethod.java
@@ -4,7 +4,7 @@ public class MainMethod {
 	public static void main(String[] args) {
 
 		System.out.println("This is a test to make sure you can pull from a repo.");
-		System.out.println("Change this line to something else. Then commit & push to your repo.");
+		System.out.println("This is Tate's custom line?");
 
 	}
 
